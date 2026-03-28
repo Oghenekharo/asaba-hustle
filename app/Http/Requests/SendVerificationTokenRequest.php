c91e2b3e106249664b94308d45911daab2bb67f3
OgheneKharo <oghenekharo@gmail.com>
2026-03-28 16:50:20 +0100

    Fixed issue with WebSocket Failure and Phone Verification

--- app/Http/Requests/SendVerificationTokenRequest.php
+++ app/Http/Requests/SendVerificationTokenRequest.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SendVerificationTokenRequest extends FormRequest
 {
+    protected function prepareForValidation(): void
+    {
+        if (!$this->filled('channel')) {
+            $this->merge([
+                'channel' => 'phone',
+            ]);
+        }
+    }
+
     public function authorize(): bool
     {
         return true;
